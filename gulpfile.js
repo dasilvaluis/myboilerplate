99fa6ff795bb77735c75df63eb0af627462cc53f
Luís Silva <luismiguel.s91@gmail.com>
2016-12-02 23:37:12 +0000

    full integration with config.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,24 @@
 var autoPrefixBrowserList = ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'];
 
-var gulp            = require('gulp'),
-    sass            = require('gulp-sass'),
-    browserSync     = require('browser-sync'),
-    autoprefixer    = require('gulp-autoprefixer'),
-    uglify          = require('gulp-uglify'),
-    cssnano         = require('gulp-cssnano'),
-    concat          = require('gulp-concat'),
-    sourceMaps      = require('gulp-sourcemaps'),
-    imagemin        = require('gulp-imagemin'),
-    gulpSequence    = require('gulp-sequence').use(gulp),
-    plumber         = require('gulp-plumber'),
-    clean           = require('gulp-clean'),
-    dotenv          = require('dotenv').config(),	 
-    notify          = require('gulp-notify'),
-    gulpif          = require('gulp-if');
-    argv            = require('minimist')(process.argv.slice(2)),
-    filelog         = require('gulp-filelog'),
-    jshint          = require('gulp-jshint');
+var gulp            = require('gulp');
+var sass            = require('gulp-sass');
+var browserSync     = require('browser-sync');
+var autoprefixer    = require('gulp-autoprefixer');
+var uglify          = require('gulp-uglify');
+var cssnano         = require('gulp-cssnano');
+var concat          = require('gulp-concat');
+var sourcemaps      = require('gulp-sourcemaps');
+var imagemin        = require('gulp-imagemin');
+var gulpSequence    = require('gulp-sequence').use(gulp);
+var plumber         = require('gulp-plumber');
+var clean           = require('gulp-clean');
+var dotenv          = require('dotenv').config();	 
+var notify          = require('gulp-notify');
+var gulpif          = require('gulp-if');
+var argv            = require('minimist')(process.argv.slice(2));
+var filelog         = require('gulp-filelog');
+var jshint          = require('gulp-jshint');
+var lazypipe        = require('lazypipe');
 
 var prod    = argv.production,
     lintjs  = argv.lintjs;
@@ -32,55 +33,62 @@ var config  = require('./assets/config.json'),
  * @param {string} def Default dependency to be injected in the end
  * @returns Array of dependencies paths
  */
-var sources = function(list, def){
+var sources = function (list){
     var sources = [];
     list.forEach(function(dep) {
         sources.push(paths.source + dep);
     });
-    if(def)
-        sources.push(def);
     return sources;
 };
 
+var finalize = function (directory) {
+  return lazypipe()
+    .pipe(gulp.dest, paths.dist + directory)
+    .pipe(browserSync.stream)();
+};
+
 gulp.task('images', function () {
-    gulp.src(paths.source + 'images/**/*')
+    gulp.src(sources(deps.images))
         .pipe(gulpif(prod, imagemin({
             optimizationLevel: 5,
             progressive: true,
             interlaced: true
         })))
-        .pipe(gulp.dest(paths.dist + 'images'))
-        .pipe(browserSync.stream());
+        .pipe(finalize('images'));
 });
 
 gulp.task('fonts', function () {
     
-    return gulp.src(sources(deps.fonts, paths.source + 'fonts/**/*'))
-        .pipe(gulp.dest(paths.dist + 'fonts'))
-        .pipe(browserSync.stream());
+    return gulp.src(sources(deps.fonts))
+        .pipe(finalize('fonts'));
+});
+
+gulp.task('jshint', function() {
+  return gulp.src(['gulpfile.js', 'assets/config.js'].concat(deps.js))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
 gulp.task('scripts', function() {
 
-    return gulp.src(sources(deps.js, paths.source + 'scripts/main.js'))
-        .pipe(gulpif(lintjs, jshint()))
-        .pipe(gulpif(lintjs, jshint.reporter('default')))
+    return gulp.src(sources(deps.js))
         .pipe(plumber({
             errorHandler: notify.onError('Error: <%= error.message %>')
         }))
+        .pipe(gulpif(!prod, sourcemaps.init()))
         .pipe(concat('main.js'))
+        .pipe(gulpif(!prod, sourcemaps.write('.')))
         .pipe(gulpif(prod, uglify()))
-        .pipe(gulp.dest(paths.dist + 'scripts' ))
-        .pipe(browserSync.stream());
+        .pipe(finalize('scripts'));
 });
 
 gulp.task('styles', function() {
 
-    return gulp.src(sources(deps.css, paths.source + 'styles/main.scss'))
+    return gulp.src(sources(deps.css))
         .pipe(plumber({
             errorHandler: notify.onError('Error: <%= error.message %>')
         }))
-        .pipe(gulpif(!prod, sourceMaps.init()))
+        .pipe(gulpif(!prod, sourcemaps.init()))
         .pipe(gulpif('*.scss', sass({
             errLogToConsole: true,
             includePaths: ['.']
@@ -89,15 +97,14 @@ gulp.task('styles', function() {
             browsers: autoPrefixBrowserList,
             cascade:  true
         }))
-        .pipe(gulpif(!prod, sourceMaps.write()))
-        .pipe(gulpif(prod, cssnano()))
         .pipe(concat('main.css'))
-        .pipe(gulp.dest(paths.dist + 'styles' ))
-        .pipe(browserSync.stream());
+        .pipe(gulpif(!prod, sourcemaps.write('.')))
+        .pipe(gulpif(prod, cssnano()))
+        .pipe(finalize('styles'));
 });
 
 gulp.task('default', function(callback){
-    gulpSequence('clean', 'images', 'fonts', 'scripts', 'styles')(callback)
+    gulpSequence('clean', 'images', 'fonts', 'scripts', 'styles')(callback);
 });
 
 gulp.task('clean', function () {
@@ -115,6 +122,6 @@ gulp.task('watch', ['default'], function() {
     gulp.watch(paths.source + 'config.json', ['default']);
     gulp.watch(paths.source + 'images/**/*', ['images']);
     gulp.watch(paths.source + 'fonts/**/*', ['fonts']);
-    gulp.watch(paths.source + 'scripts/**/*', ['scripts']);
+    gulp.watch(paths.source + 'scripts/**/*', ['jshint', 'scripts']);
     gulp.watch(paths.source + 'styles/**/*', ['styles']);
 });
